ad65f1d591e80474f1fe0e704712b944467617ae
project <igorshamrov595@gmail.com>
2024-05-12 17:39:31 +0300

    .

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime, date, timedelta
 import random
 from flask_login import UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Задайте ваш ключ для сессий
@@ -12,14 +13,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parskats.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login_register'
 
 
 # Модель пользователя
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 @login_manager.user_loader
@@ -30,30 +42,47 @@ def load_user(user_id):
 # Модель для финансовых записей
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item_type = db.Column(db.String(20))
-    category = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    amount = db.Column(db.Float)
-    date = db.Column(db.Date)
+    item_type = db.Column(db.String(20), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     # Добавляем связь с пользователем
     user = db.relationship('User', backref=db.backref('items', lazy=True))
 
 
-# Страница авторизации
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/login_register', methods=['GET', 'POST'])
+def login_register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
-            login_user(user)
-            return redirect(url_for('index'))
-        else:
-            flash('Неправильное имя пользователя или пароль', 'error')
-    return render_template('login.html')
+        form_type = request.form['form_type']  # Определение типа формы (регистрация или авторизация)
+        
+        if form_type == 'register':  # Регистрация нового пользователя
+            username = request.form['username']
+            password = request.form['password']
+            existing_user = User.query.filter_by(username=username).first()
+            if existing_user:
+                flash('Lietotāja vārds jau ir aizņemts!', 'error')
+            else:
+                new_user = User(username=username, password=password)
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Jūs veisksmīgi reģistrējušies!', 'success')
+                return redirect(url_for('login_register'))
+        
+        elif form_type == 'login':  # Авторизация пользователя
+            username = request.form['username']
+            password = request.form['password']
+            user = User.query.filter_by(username=username).first()
+            if user and user.verify_password(password):
+                login_user(user)
+                flash('Jūs veiksmīgi iejejāt sistēmā!', 'success')
+                return redirect(url_for('index'))
+            else:
+                flash('Nepareizs lietotāja vārds vai parole', 'error')
+
+    return render_template('login_register.html')
 
 
 # Страница выхода из учетной записи
@@ -61,31 +90,13 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login_register'))
 
 
 # Главная страница
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-# Страница регистрации
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            flash('Это имя пользователя уже используется', 'error')
-        else:
-            new_user = User(username=username, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Вы успешно зарегистрировались', 'success')
-            return redirect(url_for('login'))
-    return render_template('register.html')
 
 
 # Страница финансового отчета
@@ -111,8 +122,8 @@ def pievienosana():
         db.session.add(new_item)
         db.session.commit()
 
-        flash('Финансовая запись успешно добавлена', 'success')
-        return redirect(url_for('pievienosana'))  # Перенаправляем на страницу с финансовым отчетом после добавления записи
+        flash('Darījums veiskmīgi pievienots!', 'success')
+        return redirect(url_for('pievienosana'))
     else:
         return render_template('pievienosana.html')
 
@@ -259,5 +270,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-
